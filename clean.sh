67555ff1eb50a411dea1c94a82b1391f508d3d56
Idriss Neumann <idriss.neumann@comwork.io>
2022-10-25 11:15:07 +0100

    Skip the first line and missing -

--- clean.sh
+++ clean.sh
@@ -5,7 +5,11 @@ CRD="perconaservermongodbbackups.psmdb.percona.com"
 K8S_CLI="kubectl"
 
 "${K8S_CLI}" -n "${NS}" get "${CRD}" | while read name trash; do
+  if [[ $name == "NAME" ]]; then
+    continue
+  fi
+
   echo "Deleting ${name}"
-  "${K8S_CLI}" -n "${NS}" get "${CRD}" "${name}" -o yaml | yq e ".metadata.finalizers = []" | kubectl -n "${NS}" apply -f -
+  "${K8S_CLI}" -n "${NS}" get "${CRD}" "${name}" -o yaml | yq e ".metadata.finalizers = []" - | kubectl -n "${NS}" apply -f -
   "${K8S_CLI}" -n "${NS}" delete "${CRD}" "${name}" >/dev/null 2>&1 || :
 done
