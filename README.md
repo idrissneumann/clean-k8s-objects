e20db8218370300d4413b200fd266eba153ecca0
Idriss Neumann <idriss.neumann@comwork.io>
2022-10-25 11:31:05 +0100

    Add a prefix filter

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # clean-k8s-objects
 
 Remove finilizers and delete all the object related to a CRD in a given namespace
+
+```shell
+./clean.sh -n <NAMESPACE> -c <CRD>
+```
+
+If you want to only delete the resource that begin with a prefix filter:
+
+```shell
+./clean.sh -n <NAMESPACE> -c <CRD> -f <FILTER>
+```
--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 K8S_CLI="kubectl"
+FILTER=""
 
 error() { 
   echo "Error: invalid parameters!" >&2 
@@ -12,12 +13,17 @@ usage() {
   echo "Usage: ./clean.sh -n <NAMESPACE> -c <CRD>"
   echo "-n <NAMESPACE>: Kubernetes namespace" 
   echo "-c <CRD>: Kubernetes custom resource definition"
+  echo "-f <FILTER>: Prefix filter (perform only on name begining with filter)"
   echo "-h: Get the help"
 } 
 
 main() {
+  if [[ ! $NS ]] || [[ ! $CRD ]]; then
+    error
+  fi
+
   "${K8S_CLI}" -n "${NS}" get "${CRD}" | while read name trash; do
-    if [[ $name == "NAME" ]]; then
+    if [[ $name == "NAME" ]] || [[ ! $name =~ "${FILTER}".* ]]; then
       continue
     fi
 
@@ -29,10 +35,11 @@ main() {
 
 [[ $# -lt 1 ]] && error 
 
-while getopts ":n:c:h" option; do 
+while getopts ":n:c:f:h" option; do 
     case "$option" in 
         n) NS="${OPTARG}" ;; 
         c) CRD="${OPTARG}" ;; 
+        f) FILTER="${OPTARG}" ;;
         :) error ;;
         h) usage ;; 
         *) error ;; 
